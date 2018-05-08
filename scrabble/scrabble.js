853b1f1f30932ac33c6b0047c140e1857d1d7183
James Logue <logueja@gmail.com>
2018-05-08 14:08:33 -0600

    Add scrabble code challenge

---
+++ scrabble/scrabble.js
@@ -0,0 +1,28 @@
+//Given a random set of 7 Scrabble tiles, and a fixed position, what word in the English dictionary will result in the most points.
+
+//Your code should take three inputs: a list of 7 random letters, a constant letter, and the position in the result word where that constant letter exists. For example:
+
+//best_scrabble_word('AJDUKWP', 'C', 3)
+
+//Using the 7 random letters, you must find which word in the English dictionary will calculate the high points, with a restriction that the letter 'C' is in the 3rd position. In this scenario, perhaps the words JACK, DUCK, and PUCK are all found, but your code will need to also load the point values for each letter. In this example, JACK is work the most points.
+
+//Edge case: if a SPACE character is given in the list of 7 tiles, it is a wildcard that can be any letter in a word, but you do not receive points for that letter.
+
+//English-language editions of Scrabble contain 100 letter tiles, in the following distribution:
+
+//2 blank tiles (scoring 0 points)
+//1 point: E, A, I, L, N, O, R, S, T, U
+//2 points: D, G
+//3 points: B, C, M, P
+//4 points: F, H, V, W, Y
+//5 points: K
+//8 points: J, X
+//10 points: Q, Z
+//Use /usr/share/dict/words on your system to do English word lookups.
+
+
+//PSEUDOCODE
+//  hash to track points
+//  
+
+console.log('i am a js file executing')
