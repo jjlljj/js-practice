0f8596b95bbcc9a54135ddbdab303a8e809002cf
James Logue <logueja@gmail.com>
2018-05-08 15:44:09 -0600

    Regex solution

--- scrabble/scrabble.js
+++ scrabble/scrabble.js
@@ -20,9 +20,36 @@
 //10 points: Q, Z
 //Use /usr/share/dict/words on your system to do English word lookups.
 
+const fs = require('fs');
+const dictionary = fs.readFileSync('/usr/share/dict/words').toString().trim().split('\n')
+const charVal = { e: 1, a: 1, i: 1, l: 1, n: 1, o: 1, r: 1, s: 1, t: 1, u:1, d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y:4, k: 5, j: 8, x: 8, q: 10, z:10  }
 
-//PSEUDOCODE
-//  hash to track points
-//  
+function best_scrabble_word(chars, char, pos) {
+  if ( char ) {
+    chars += char.toLowerCase()
+  }
+  chars.replace(/\s/g, '.')
+  const regExp = new RegExp("^[" + chars.toLowerCase() + "]+$", "i", "g")
+  const possibleWords = dictionary.filter(word => word.match(regExp) && word.length <= chars.length)
 
-console.log('i am a js file executing')
+  if (char && pos) {
+    const wordsWithPosition = possibleWords.filter(word =>  word[parseInt(pos)-1] == char.toLowerCase())
+    return best_word(wordsWithPosition)
+  }
+  return best_word(possibleWords)
+}
+
+function best_word(words) {
+  return words.map(word => {
+    return { word, score: score_word(word) }
+  }).sort((a, b) => a.score > b.score)[0]
+}
+
+function score_word(word) {
+  return word.toLowerCase().split('').reduce((score, char) => {
+    return score + charVal[char]
+  }, 0)
+
+}
+
+console.log(best_scrabble_word('ajdukwp', 'C', '3'))
